cdc4b46b8a55d8f34d4274b24403074672f627c3
Paul Gierz <pgierz@awi.de>
2019-08-13 10:24:52 +0200

    first copytree

--- functions/general_py/esm_parser.py
+++ functions/general_py/esm_parser.py
@@ -14,6 +14,7 @@ import collections
 import logging
 import os
 import re
+import shutil
 import socket
 import warnings
 
@@ -922,9 +923,9 @@ class GeneralConfig(dict):
         for attachment in CONFIGS_TO_ALWAYS_ATTACH_AND_REMOVE:
             attach_to_config_and_remove(self.config, attachment)
         self._config_init()
-        # for k, v in self.config.items():
-        #    self.__setitem__(k, v)
-        # del self.config
+        for k, v in self.config.items():
+            self.__setitem__(k, v)
+        del self.config
 
     def _config_init(self):
         raise NotImplementedError(
@@ -938,6 +939,118 @@ class ConfigTest(GeneralConfig):
             "computer": yaml_file_to_dict(determine_computer_from_hostname())
         }
         self.config = merge_dicts(setup_relevant_configs, self.config)
+
+
+class SimulationSetup(object):
+    def __init__(self, name):
+        self.config = ConfigSetup(name)
+        pprint_config(self.config)
+        components = []
+        for component in self.config["submodels"]:
+            components.append(SimulationComponent(self.config[component]))
+
+
+class SimulationComponent(object):
+    def __init__(self, config):
+        self.config = config
+
+        self.exp_base = "/tmp/example_experiments/"
+        self.expid = "test"
+        start_date = "18500101"
+        end_date = "18510101"
+
+        self.all_filetypes = [
+            "forcing",
+            "config",
+            "input",
+            "restart",
+            "outdata",
+            "log",
+            "mon",
+            "analysis",
+            "bin",
+            "viz",
+        ]
+
+        for filetype in self.all_filetypes:
+            setattr(
+                self,
+                "thisrun_" + filetype + "_dir",
+                self.exp_base
+                + "/"
+                + self.expid
+                + "/run_"
+                + start_date
+                + "-"
+                + end_date
+                + "/"
+                + filetype
+                + "/"
+                + self.config["model"]
+                + "/",
+            )
+            if not os.path.exists(getattr(self, "thisrun_" + filetype + "_dir")):
+                os.makedirs(getattr(self, "thisrun_" + filetype + "_dir"))
+
+            setattr(
+                self,
+                "experiment_" + filetype + "_dir",
+                self.exp_base
+                + "/"
+                + self.expid
+                + "/"
+                + filetype
+                + "/"
+                + self.config["model"]
+                + "/",
+            )
+            if not os.path.exists(getattr(self, "experiment_" + filetype + "_dir")):
+                os.makedirs(getattr(self, "experiment_" + filetype + "_dir"))
+
+    def filesystem_to_experiment(self):
+        for filetype in self.all_filetypes:
+            # forcing_files = {'sst': 'pisst'}
+            # forcing_sources = {'pisst': '/some/path/to/pisst_file/'}
+            # forcing_in_work = {'sst': 'unit.24'}
+            if filetype + "_sources" not in self.config:
+                continue
+            filedir_intermediate = getattr(self, "thisrun_" + filetype + "_dir")
+            for file_descriptor, file_source in self.config[
+                filetype + "_sources"
+            ].items():
+                if filetype + "_files" in self.config:
+                    if file_descriptor not in self.config[filetype + "_files"].values():
+                        continue
+                    else:
+                        inverted_dict = {
+                            v: k for k, v in self.config[filetype + "_files"].items()
+                        }
+                        file_category = inverted_dict[file_descriptor]
+                else:
+                    file_category = file_descriptor
+                if (
+                    filetype + "_in_work" in self.config
+                    and file_category in self.config[filetype + "_in_work"].keys()
+                ):
+                    # Cosmetic TODO: Give this dude a real name
+                    file_target = (
+                        filedir_intermediate
+                        + "/"
+                        + self.config[filetype + "_in_work"][file_category]
+                    )
+                else:
+                    file_target = (
+                        filedir_intermediate + "/" + os.path.basename(file_source)
+                    )
+                shutil.copy2(
+                    file_source,
+                    filedir_intermediate + "/" + os.path.basename(file_source),
+                )
+                if not os.path.isfile(file_target):
+                    os.symlink(
+                        filedir_intermediate + "/" + os.path.basename(file_source),
+                        file_target,
+                    )
 
 
 class ConfigSetup(GeneralConfig):
@@ -1102,6 +1215,6 @@ if __name__ == "__main__":
     )
 
     if ARGS.setup:
-        CFG = ConfigSetup(ARGS.setup)
+        SETUP = SimulationSetup(ARGS.setup)
         yaml.Dumper.ignore_aliases = lambda *args: True
-        print(yaml.dump(CFG, default_flow_style=False))
+        print(yaml.dump(SETUP.config, default_flow_style=False))
